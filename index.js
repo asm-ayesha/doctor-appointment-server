7d638b28ee441a4e7f5f732a4d6780e70b6cf902
asm-ayesha <ayesha219333@gmail.com>
2026-05-18 20:42:23 +0600

    install cord

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const dns = require('dns');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const dns = require("dns");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 dotenv.config();
 
 dns.setDefaultResultOrder("ipv4first");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const uri = process.env.DB_URI;
+
+// ✅ Middleware
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
-    // await client.db("admin").command({ ping: 1 });
+
     const db = client.db("doctor-appoinment");
-    const doctorCollection = db.collection("doctors")
+    const doctorCollection = db.collection("doctors");
 
-
-    app.get("/doctors", async(req, res)=>{
-        const cursor = doctorCollection.find();
-        const result = await cursor.toArray();
-
-        res.send(result)
+    // ✅ Get all doctors
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorCollection.find().toArray();
+      res.send(result);
     });
 
+    // ✅ Get single doctor
+    app.get("/doctors/:doctorId", async (req, res) => {
+      const doctorId = req.params.doctorId;
 
+      const query = {
+        _id: new ObjectId(doctorId),
+      };
 
-    app.get("/doctors/:doctorId", async(req, res) =>{
-        const doctorId = req.params.doctorId;
+      const result = await doctorCollection.findOne(query);
 
-        const query = {_id: new ObjectId(doctorId)}
-        console.log(query)
+      res.send(result);
+    });
 
-        const result = await doctorCollection.findOne(query);
-        res.send(result)
-    })
-
-
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
   }
 }
-run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World! hellow');
+run();
+
+app.get("/", (req, res) => {
+  res.send("Server running");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
